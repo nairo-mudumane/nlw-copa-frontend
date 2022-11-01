0dddc25782e0ec84b6de964f44addc3662f3b676
nairo-mudumane <nairomudumane@outlook.com>
2022-11-01 12:33:19 +0200

    project reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>app</div>;
+interface HomeProps {
+  count: number;
 }
+
+export default function Home(props: HomeProps) {
+  return <div>count: {props.count}</div>;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count");
+  const data = await response.json();
+
+  return {
+    props: { count: data.data },
+  };
+};
